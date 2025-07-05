e39de9ac68bace9678b00681335bf9d1e68db0c1
Josh-dotcom-prog <edyangujoshua@gmail.com>
2025-07-05 18:58:43 +0300

    learnt how to use eloquent models, creating models and migrations

--- example/routes/web.php
+++ example/routes/web.php
@@ -4,16 +4,18 @@ use Illuminate\Support\Facades\Route;
 use illuminate\Support\Arr;
 use App\Models\Job;
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/', function () {
+   return view('index');
 });
 
+//Retrieves all jobs from the database
 Route::get('/jobs', function () {
     return view('jobs', [
         'jobs' => Job::all()
     ]);
 });
 
+//Finds Job by id
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('job', ['job' => $job]);
